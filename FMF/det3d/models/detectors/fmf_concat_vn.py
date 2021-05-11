899ffd4941379eb3b85302b0653a72c71202723c
youshaamurhij <yosha.morheg@gmail.com>
2021-05-11 11:38:20 +0300

    fmf two sage bug fixed

--- FMF/det3d/models/detectors/fmf_concat_vn.py
+++ FMF/det3d/models/detectors/fmf_concat_vn.py
@@ -88,6 +88,15 @@ class FMF_Concat_VN(SingleStageDetector):
 
         x, voxel_feature = self.extract_feat(data)
         bev_feature = x 
+
+        if x.shape == self.tensor.shape:
+            x1 = cat((x,self.tensor),1)
+        else:
+            x1 = cat((x[0].view(1,512,188,-1),self.tensor[0].view(1,512,188,-1)),1)
+            
+        self.tensor = x.detach().clone()
+        
+        x = self.shared_conv(x1)
         preds = self.bbox_head(x)
 
         # manual deepcopy ...
