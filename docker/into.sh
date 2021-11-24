754ac16facf527cd02d108cd80ba3e0087a45063
youshaamurhij <yosha.morheg@gmail.com>
2021-11-24 15:49:36 +0300

    added singularity container

--- docker/into.sh
+++ docker/into.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker exec -it fmf_nusc \
+docker exec -it fmf \
     /bin/bash -c "export PYTHONPATH=\"${PYTHONPATH}:/home/trainer/fmf/FMF\";
     export PYTHONPATH=\"${PYTHONPATH}:/home/trainer/fmf/nuscenes-devkit/python-sdk\";
     cd /home/trainer/fmf/FMF;
--- docker/start.sh
+++ docker/start.sh
@@ -5,27 +5,24 @@ cd ..
 
 workspace_dir=$PWD
 
-if [ "$(docker ps -aq -f status=exited -f name=fmf_nusc)" ]; then
-    docker rm fmf_nusc;
+if [ "$(docker ps -aq -f status=exited -f name=fmf)" ]; then
+    docker rm fmf;
 fi
 
 docker run -it -d --rm \
-    --gpus '"device=0,1"' \
+    --gpus '"device=1"' \
     --net host \
     -e "NVIDIA_DRIVER_CAPABILITIES=all" \
     -e "DISPLAY" \
     -e "QT_X11_NO_MITSHM=1" \
     --shm-size="45g" \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    --name fmf_nusc \
+    --name fmf \
     -v $workspace_dir/:/home/trainer/fmf:rw \
-    -v /home/josh94mur/Documents/:/home/SUOD:rw \
-    -v /datasets/waymo_perception/:/home/trainer/fmf/FMF/data/Waymo:rw \
-    -v /datasets/waymo_perception/:/home/SUOD/CenterPoint/data/Waymo:rw \
-    -v /datasets/nuScenes/:/home/trainer/fmf/FMF/data/nuScenes:rw \
+    -v /media/josh/HDD-1TB3/Waymo/:/home/trainer/fmf/FMF/data/Waymo:rw \
     x64/fmf:latest
 
-docker exec -it fmf_nusc \
+docker exec -it fmf \
     /bin/bash -c "export PYTHONPATH=\"${PYTHONPATH}:/home/trainer/fmf/FMF\";
     export PYTHONPATH=\"${PYTHONPATH}:/home/trainer/fmf/nuscenes-devkit/python-sdk\";
     cd /home/trainer/fmf/FMF;
